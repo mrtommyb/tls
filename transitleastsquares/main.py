6a9c89b2f73d63db978fe8ce2498fed46f844b32
mrtommyb <tom@tombarclay.com>
2024-12-25 13:16:21 -0500

    making a minimal version

--- transitleastsquares/main.py
+++ transitleastsquares/main.py
@@ -48,6 +48,200 @@ class transitleastsquares(object):
         self.t, self.y, self.dy = validate_inputs(t, y, dy)
         self.verbose = verbose
 
+    def power_limited(self, **kwargs):
+        """Compute the periodogram for a set of user-defined parameters, with minimal returns"""
+        self, kwargs = validate_args(self, kwargs)
+
+        if self.verbose:
+            print(tls_constants.TLS_VERSION)   
+
+        periods = period_grid(
+            R_star=self.R_star,
+            M_star=self.M_star,
+            time_span=numpy.max(self.t) - numpy.min(self.t),
+            period_min=self.period_min,
+            period_max=self.period_max,
+            oversampling_factor=self.oversampling_factor,
+            n_transits_min=self.n_transits_min,
+        )
+
+        durations = duration_grid(
+            periods, shortest=1 / len(self.t), log_step=self.duration_grid_step
+        )
+
+        maxwidth_in_samples = int(numpy.max(durations) * numpy.size(self.y))
+        if maxwidth_in_samples % 2 != 0:
+            maxwidth_in_samples = maxwidth_in_samples + 1
+        lc_cache_overview, lc_arr = get_cache(
+            durations=durations,
+            maxwidth_in_samples=maxwidth_in_samples,
+            per=self.per,
+            rp=self.rp,
+            a=self.a,
+            inc=self.inc,
+            ecc=self.ecc,
+            w=self.w,
+            u=self.u,
+            limb_dark=self.limb_dark,
+            verbose=self.verbose
+        )
+
+        if self.verbose:
+            print(
+                "Searching "
+                + str(len(self.y))
+                + " data points, "
+                + str(len(periods))
+                + " periods from "
+                + str(round(min(periods), 3))
+                + " to "
+                + str(round(max(periods), 3))
+                + " days"
+            )
+
+        if self.verbose:
+            if self.use_threads == multiprocessing.cpu_count():
+                print("Using all " + str(self.use_threads) + " CPU threads")
+            else:
+                print(
+                    "Using "
+                    + str(self.use_threads)
+                    + " of "
+                    + str(multiprocessing.cpu_count())
+                    + " CPU threads"
+                )
+
+        if self.show_progress_bar:
+            bar_format = "{desc}{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} periods | {elapsed}<{remaining}"
+            pbar = tqdm(total=numpy.size(periods), smoothing=0.3, bar_format=bar_format)
+
+        if tls_constants.PERIODS_SEARCH_ORDER == "ascending":
+            periods = reversed(periods)
+        elif tls_constants.PERIODS_SEARCH_ORDER == "descending":
+            pass  # it already is
+        elif tls_constants.PERIODS_SEARCH_ORDER == "shuffled":
+            periods = numpy.random.permutation(periods)
+        else:
+            raise ValueError("Unknown PERIODS_SEARCH_ORDER")
+
+        # Result lists now (faster), convert to numpy array later
+        test_statistic_periods = []
+        test_statistic_residuals = []
+        test_statistic_rows = []
+        test_statistic_depths = []
+
+        if self.use_threads > 1:  # Run multi-core search
+            pool = multiprocessing.Pool(processes=self.use_threads)
+            params = partial(
+                search_period,
+                t=self.t,
+                y=self.y,
+                dy=self.dy,
+                transit_depth_min=self.transit_depth_min,
+                R_star_min=self.R_star_min,
+                R_star_max=self.R_star_max,
+                M_star_min=self.M_star_min,
+                M_star_max=self.M_star_max,
+                lc_arr=lc_arr,
+                lc_cache_overview=lc_cache_overview,
+                T0_fit_margin=self.T0_fit_margin,
+            )
+            for data in pool.imap_unordered(params, periods):
+                test_statistic_periods.append(data[0])
+                test_statistic_residuals.append(data[1])
+                test_statistic_rows.append(data[2])
+                test_statistic_depths.append(data[3])
+                if self.show_progress_bar:
+                    pbar.update(1)
+            pool.close()
+        else:
+            for period in periods:
+                data = search_period(
+                    period=period,
+                    t=self.t,
+                    y=self.y,
+                    dy=self.dy,
+                    transit_depth_min=self.transit_depth_min,
+                    R_star_min=self.R_star_min,
+                    R_star_max=self.R_star_max,
+                    M_star_min=self.M_star_min,
+                    M_star_max=self.M_star_max,
+                    lc_arr=lc_arr,
+                    lc_cache_overview=lc_cache_overview,
+                    T0_fit_margin=self.T0_fit_margin,
+                )
+                test_statistic_periods.append(data[0])
+                test_statistic_residuals.append(data[1])
+                test_statistic_rows.append(data[2])
+                test_statistic_depths.append(data[3])
+                if self.show_progress_bar:
+                    pbar.update(1)
+
+        if self.show_progress_bar:
+            pbar.close()
+
+        # imap_unordered delivers results in unsorted order ==> sort
+        test_statistic_periods = numpy.array(test_statistic_periods)
+        sort_index = numpy.argsort(test_statistic_periods)
+        test_statistic_periods = test_statistic_periods[sort_index]
+        test_statistic_residuals = numpy.array(test_statistic_residuals)[sort_index]
+        test_statistic_rows = numpy.array(test_statistic_rows)[sort_index]
+        test_statistic_depths = numpy.array(test_statistic_depths)[sort_index]
+
+        idx_best = numpy.argmin(test_statistic_residuals)
+        best_row = test_statistic_rows[idx_best]
+        duration = lc_cache_overview["duration"][best_row]
+        maxwidth_in_samples = int(numpy.max(durations) * numpy.size(self.t))
+
+        if max(test_statistic_residuals) == min(test_statistic_residuals):
+            no_transits_were_fit = True
+            warnings.warn('No transit were fit. Try smaller "transit_depth_min"')
+        else:
+            no_transits_were_fit = False
+
+        # Power spectra variants
+        chi2 = test_statistic_residuals
+
+        if no_transits_were_fit:
+            period = numpy.nan
+            depth = 1
+            SDE = 0
+            T0 = 0
+            duration = numpy.nan
+        else:
+            SR, power_raw, power, SDE_raw, SDE = spectra(chi2, self.oversampling_factor)
+            index_highest_power = numpy.argmax(power)
+            period = test_statistic_periods[index_highest_power]
+            depth = test_statistic_depths[index_highest_power]
+            T0 = final_T0_fit(
+                signal=lc_arr[best_row],
+                depth=depth,
+                t=self.t,
+                y=self.y,
+                dy=self.dy,
+                period=period,
+                T0_fit_margin=self.T0_fit_margin,
+                show_progress_bar=self.show_progress_bar,
+                verbose=self.verbose
+            )
+            transit_times = all_transit_times(T0, self.t, period)
+
+            transit_duration_in_days = calculate_transit_duration_in_days(
+                self.t, period, transit_times, duration
+            )
+            duration = transit_duration_in_days
+
+        return transitleastsquaresresults(
+            SDE,
+            period,
+            T0,
+            duration,
+            depth,
+        )              
+
+
+
+
     def power(self, **kwargs):
         """Compute the periodogram for a set of user-defined parameters"""
         self, kwargs = validate_args(self, kwargs)
